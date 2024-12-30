da2f8a29b7d7367e769d4ae50a8e791b6905c5b5
lalitverma01 <lalitverma2923@gmail.comm>
2024-12-30 12:46:02 +0530

    js sidebar

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+function navopen() {
+    document.getElementById("sidebar").style.width = "286px";
+    document.getElementById("main").style.backgroundColor = "#B9B9B9";
+}
+function navclose() {
+    document.getElementById("sidebar");
+    document.getElementById("opensidebar");
+    document.getElementById("sidebar").style.width = "0";
+    document.getElementById("main").style.backgroundColor = "white";
+}
