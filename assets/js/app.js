21237b737011575361db06ff4720619942543658
lalitverma01 <lalitverma2923@gmail.comm>
2024-12-31 11:48:28 +0530

    add bg-layer

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,11 @@
 function navopen() {
     document.getElementById("sidebar").style.width = "286px";
-    document.getElementById("main").style.backgroundColor = "#B9B9B9";
+    document.getElementById("sidebar").style.display = "block";
+    document.getElementById("bg-layer").style.display = "block";
 }
+
 function navclose() {
-    document.getElementById("sidebar");
     document.getElementById("sidebar").style.width = "0";
-    document.getElementById("main").style.backgroundColor = "white";
+    document.getElementById("bg-layer").style.display = "none";
+
 }
